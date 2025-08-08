cb2ad4cbb11f04f49170bd4398addfa609f585f2
Fernando S <51276791+beztao01@users.noreply.github.com>
2025-08-07 18:44:14 -0600

    se actualizo el script

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
